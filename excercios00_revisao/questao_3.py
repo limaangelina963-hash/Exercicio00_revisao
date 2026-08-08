470ddd2745efc592f0cafd2367520309bf62a22b
limaangelina963-hash <limaangelina963@gmail.com>
2026-08-07 21:47:05 -0300

    Add files via upload

---
+++ excercios00_revisao/questao_3.py
@@ -0,0 +1,15 @@
+periodo = input("digite  M para turno  matutino, V para  verpetinoe N para noturno: ")
+
+if periodo == "M":
+    print("Bom dia! ")
+elif periodo == "V":
+    print("Boa tarde! ")
+elif periodo == "N":
+    print("Boa noite! ")
+
+else:print("valor inválido! ")
+
+
+
+
+
---
+++ excercios00_revisao/questao_5.py
@@ -0,0 +1,8 @@
+numero = int(input("digite um número: "))
+
+if numero > 0:
+    print("0 número é positivo ")
+elif numero < 0:
+    print("0 número é negativo ")
+else:
+    print("o número é zero ")
